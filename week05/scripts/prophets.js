28c5c63ac2211b66028a3681c9119a1f9463c4b1
Kakeru-Kikuta <kk1911shoter@icloud.com>
2023-11-28 17:16:29 +0900

    css removed

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,10 +1,11 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
 
-async function getProphetData() {
+async function getProphetData(url) {
     const response = await fetch(url);
     const data = await response.json();
     // console.table(data.prophets);
+    displayProphets(data.prophets);
   }
   
   getProphetData();
@@ -32,3 +33,5 @@ const displayProphets = (prophets) => {
         cards.appendChild(card);
     }); // end of arrow function and forEach loop
   }
+
+displayProphets();
