aee39e0dcb6fcda5be12fc892168343cb1baad9c
Kakeru-Kikuta <kk1911shoter@icloud.com>
2023-11-30 12:37:34 +0900

    weather added

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -56,28 +56,23 @@ localStorage.setItem("numVisits-ls", numVisits);
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDese = document.querySelector("figcaption");
-
-const url = ""
-async function apiFetch () {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data =await response.json();
-            console.log(data);
-        } else {
-            throw Error(await response.text());
-        } catch (error) {
-            console.log(error);
-        }
-    }
-}
-apiFetch();
+const apikey = "24ab24a6d14827831d03fa25d028e5ef";
+const lon = "140.96140167665035"
+const lat = "38.58447280905054"
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=imperial`
 
 function displayResults(data) {
-    currentTemp.innerHTML =`${data.____}&deg;F`;
-    const incosrc = ``
-    let desc = data.weather[0].;
-    weatherIcon.setAttribute();
-    weatherIcon.setAttribute();
+    currentTemp.innerHTML =`${data.main.temp} &deg;F`;
+    const src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src",src);
     captionDese.textContent = `${desc}`;
 }
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        displayResults(jsObject);
+    });
+    
