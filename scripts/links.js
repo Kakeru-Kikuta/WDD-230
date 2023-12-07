6ea487f68a082d49a54b267861a64393d6ec9dfd
Kakeru-Kikuta <kk1911shoter@icloud.com>
2023-12-07 15:07:35 +0900

    as much as I can

--- scripts/links.js
+++ scripts/links.js
@@ -1,28 +1,24 @@
 const baceurl = "https://Kakeru-Kikuta.github.io/wdd230/";
 const linksurl = "https://Kakeru-Kikuta.github.io/wdd230/data/links.json";
 
-// async function getLinks() {
-//     const response = await fetch(linksurl);
-//     const data =await response.json();
-//     displayLinks(data);
-// }
-
 function displayLinks(weeks) {
-    let menu = document.createElement("allmenu")
-    let aweek = document.createElement("w");
-    let contents = document.createElement("content")
 
-    contents.setAttribute("li", weeks.week);
-    contents.setAttribute("href", weeks.url);
-    contents.setAttribute("a", weeks.title);
+    weeks.forEach(function(weeks) {
 
-    aweek.textContent = weeks.week;
-    ref.textContent = weeks.url;
-    tag.textContent = weeks.title;
+        let section = document.querySelector('.submenu');
 
-    menu.appendChild(aweek);
+        let w = weeks.week;
+        let l = weeks.links.url;
+        let t = weeks.links.title;
+
+        let code = '<ul class="submenu">' + 
+        '<li>' + w +
+        '<a href="'+l+'">' + t + '</a>' +
+        '</li>'
+
+        section.insertAdjacentHTML('beforeend', code);
+    });
     
-    document.querySelector(".submenu").appendChild(menu)
 }
 
 fetch(linksurl)
